e0120545029dd7df9537348f4c28be3d41386c32
CarloSalaris <carloa.salaris@gmail.com>
2023-05-12 16:53:53 +0200

    Ogni stringa in array -> object corrispondente

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ Stampare le stesse informazioni su DOM sottoforma di stringhe
 /* SVOLGIMENTO */
 
 // Creo un Array di stringhe
-
 let teamArr = [
     "Wayne Barnett,Founder & CEO,wayne-barnett-founder-ceo.jpg",
     "Angela Caroll,Chief Editor,angela-caroll-chief-editor.jpg",
@@ -26,18 +25,32 @@ let teamArr = [
     "Barbara Ramos,Graphic Designer,barbara-ramos-graphic-designer.jpg",
 ]
 
+
 // Devo "splittare" ogni stringa, passarla per un ciclo e creare le 3 proprietà correlate con il contenuto della stringa
 
 //Esempio individuale da generalizzare
-let WayneBarnettArr = teamArr[0].split(",")
-let WayneBarnettObj = {};
-for (let i = 0; i < WayneBarnettArr.length; i++) {
-    WayneBarnettObj.nome = WayneBarnettArr[0];
-    WayneBarnettObj.ruolo = WayneBarnettArr[1];
-    WayneBarnettObj.foto = WayneBarnettArr[2];   
+// let BarnettArr = teamArr[0].split(",")
+// let BarnettObj = {};
+// for (let i = 0; i < BarnettArr.length; i++) {
+//     BarnettObj.nome = BarnettArr[0];
+//     BarnettObj.ruolo = BarnettArr[1];
+//     BarnettObj.foto = BarnettArr[2];   
+// }
+
+// teamArr[0] = BarnettObj;
+
+// console.log(BarnettObj);
+// console.log(teamArr);
+
+// Ciclo per far passare il processo a tutti gli elementi del teamArr
+
+for (let i = 0; i < teamArr.length; i++) {
+    let temp = teamArr[i].split(",");
+    teamArr[i] = {};
+    teamArr[i].nome = temp[0];
+    teamArr[i].ruolo = temp[1];
+    teamArr[i].foto = temp[2];   
+
+    console.log(teamArr[i]);
 }
-
-teamArr[0] = WayneBarnettObj;
-
-console.log(WayneBarnettObj);
 console.log(teamArr);
