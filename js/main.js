1d2a60e2cfc1acef034c5bb9842d52dec3e2036c
CarloSalaris <carloa.salaris@gmail.com>
2023-05-12 17:01:24 +0200

    Ordine, commenti, refactoring

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ Stampare le stesse informazioni su DOM sottoforma di stringhe
 
 /* SVOLGIMENTO */
 
-// Creo un Array di stringhe
+// Creo un Array di stringhe (copiate e incollate da file esterno)
 let teamArr = [
     "Wayne Barnett,Founder & CEO,wayne-barnett-founder-ceo.jpg",
     "Angela Caroll,Chief Editor,angela-caroll-chief-editor.jpg",
@@ -25,32 +25,20 @@ let teamArr = [
     "Barbara Ramos,Graphic Designer,barbara-ramos-graphic-designer.jpg",
 ]
 
-
-// Devo "splittare" ogni stringa, passarla per un ciclo e creare le 3 proprietà correlate con il contenuto della stringa
-
-//Esempio individuale da generalizzare
-// let BarnettArr = teamArr[0].split(",")
-// let BarnettObj = {};
-// for (let i = 0; i < BarnettArr.length; i++) {
-//     BarnettObj.nome = BarnettArr[0];
-//     BarnettObj.ruolo = BarnettArr[1];
-//     BarnettObj.foto = BarnettArr[2];   
-// }
-
-// teamArr[0] = BarnettObj;
-
-// console.log(BarnettObj);
-// console.log(teamArr);
+/* MILESTONE 0: */
 
 // Ciclo per far passare il processo a tutti gli elementi del teamArr
-
 for (let i = 0; i < teamArr.length; i++) {
+    // "splitto" ogni stringa (array)
     let temp = teamArr[i].split(",");
+    // converto ogni elemento dell'array principale in un oggetto
     teamArr[i] = {};
+    // Aggiungo all'oggetto le proprietà e il valore rispettivo contenuto nella stringa (che è convertita in un array di stringhe = valori)
     teamArr[i].nome = temp[0];
     teamArr[i].ruolo = temp[1];
     teamArr[i].foto = temp[2];   
 
+    /* MILESTONE 1: */
     console.log(teamArr[i]);
 }
 console.log(teamArr);
